43687648859c2f494a016c54aa217adebb10564e
alae-eddine sahbou <alae.shb147@gmail.com>
2022-09-07 15:36:18 +0100

    portfolio

---
+++ src/containers/greeting/Greeting.js
@@ -0,0 +1,74 @@
+import React from "react";
+import "./Greeting.css";
+// import SocialMedia from "../../components/socialMedia/SocialMedia";
+import Button from "../../components/button/Button";
+import { greeting } from "../../portfolio";
+import { Fade } from "react-reveal";
+import BannerImg from "./BannerImg";
+import Typewriter from "typewriter-effect";
+import CompetitiveSites from "../../components/competitiveSites/CompetitiveSites";
+import { competitiveSites } from "../../portfolio";
+export default function Greeting(props) {
+  const theme = props.theme;
+  return (
+    <Fade bottom duration={2000} distance="40px">
+      <div className="greet-main" id="greeting">
+        <div className="greeting-main">
+          <div className="greeting-text-div">
+            <div>
+              <h1 className="greeting-text" style={{ color: theme.orange }}>
+                {greeting.title}
+              </h1>
+              <h1 className="greeting-sub" style={{ color: theme.grayText }}>
+                I'm <mark data-entity="person">{greeting.sub}</mark>
+              </h1>
+              <h1 className="greeting-typewriter" style={{color: theme.blue}}>
+                <Typewriter
+                  onInit={(typewriter) => {
+                    typewriter
+                      .typeString("Full Stack Developer")
+                      .pauseFor(200)
+                      .deleteAll()
+                      .typeString("Bioinformatician")
+                      .pauseFor(200)
+                      .deleteAll()
+                      .typeString("UI/UX Designer")
+                      .pauseFor(200)
+                      .deleteAll()
+                      .typeString("Biologist")
+                      .pauseFor(200)
+                      .start();
+                  }}
+                  options={{
+                    autoStart: true,
+                    loop: true,
+                  }}
+                />
+              </h1>
+              <span
+                className="greeting-text-span subTitle"
+                style={{ color: theme.text }}>
+                <div className="entities">
+                  I study as a <mark data-entity="skill">developer</mark> and a <mark data-entity="skill">biologist</mark>, my passion is programming and I like to know how things work and are composed. I am proficient in programming languages like <mark data-entity="dev">JAVA, C etc., web design with HTML5, CSS, JAVASCRIPT, PHP etc., database creation with MYSQL, graphic design, logo design etc.</mark>
+
+                </div>
+              </span>
+              <CompetitiveSites logos={competitiveSites.competitiveSites} />
+              <div className="button-greeting-div">
+                <Button text="Contact me" href="/contact" />
+                <Button
+                  text="See my resume"
+                  newTab={true}
+                  href={greeting.resumeLink}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="greeting-image-div">
+            <BannerImg />
+          </div>
+        </div>
+      </div>
+    </Fade>
+  );
+}
---
+++ src/portfolio.js
@@ -0,0 +1,602 @@
+const settings = {
+  isSplash: true, // Change this to false if you don't want Splash screen.
+};
+
+//SEO Related settings
+const seo = {
+  title: "Sahbou Alae-Eddine | Portfolio",
+  description:
+    "A Data Science practitioner who thrives to leverage startups with AI based solutions with a blend of UI/UX",
+  og: {
+    title: "Sahbou Alae-Eddine Portfolio",
+    type: "website",
+    url: "http://sahbou.ml/",
+  },
+};
+
+//Home Page
+const greeting = {
+  title: "Welcome!",
+  sub: "Sahbou Alae-Eddine",
+  logo_name: "Sahbou Alae-Eddine",
+  resumeLink: "http://sahbou.ml/assets/pdf/cv.pdf",
+};
+
+// home logos
+const competitiveSites = {
+  competitiveSites: [
+    {
+      siteName: "Github",
+      iconifyClassname: "simple-icons:github",
+      style: {
+        color: "white",
+      },
+      profileLink: "https://github.com/alaesahbou",
+    },
+    {
+      siteName: "LinkedIn",
+      iconifyClassname: "simple-icons:linkedin",
+      style: {
+        color: "#0077b5",
+      },
+      profileLink: "https://www.linkedin.com/in/alae-eddine-sahbou/",
+    },
+    {
+      siteName: "stackoverflow",
+      iconifyClassname: "simple-icons:stackoverflow",
+      style: {
+        color: "#DA7F2F",
+      },
+      profileLink: "https://stackoverflow.com/users/15875731/alae-sa",
+    },
+    {
+      siteName: "facebook",
+      iconifyClassname: "simple-icons:facebook",
+      style: {
+        color: "#4064AC",
+      },
+      profileLink: "https://www.facebook.com/alae.sa.3",
+    },
+    {
+      siteName: "instagram",
+      iconifyClassname: "simple-icons:instagram",
+      style: {
+        color: "#c13584",
+      },
+      profileLink: "https://www.instagram.com/alaesahbou/",
+    },
+    {
+      siteName: "twitter",
+      iconifyClassname: "simple-icons:twitter",
+      style: {
+        color: "#1C9CEA",
+      },
+      profileLink: "https://twitter.com/ASahbou",
+    },
+  ],
+};
+
+//what i do?
+const skills = {
+  data: [
+    {
+      title: "FULL STACK DEVELOPER",
+      fileName: "FullStackImg",
+      skills: [
+        "⚡ Building responsive websites using HTML5, CSS3, BootStrap5, ReactJS & NodeJS",
+        "⚡ Creating application backend in Node, Flask & Streamlit",
+        "⚡ Having experience in Domain & Hosting using Netlify, Vercel, Heroku & Hostinger",
+      ],
+      softwareSkills: [
+        {
+          skillName: "HTML5",
+          fontAwesomeClassname: "simple-icons:html5",
+          style: {
+            color: "#E34F26",
+          },
+        },
+        {
+          skillName: "CSS3",
+          fontAwesomeClassname: "fa-css3",
+          style: {
+            color: "#1572B6",
+          },
+        },
+        {
+          skillName: "Sass",
+          fontAwesomeClassname: "simple-icons:sass",
+          style: {
+            color: "#CC6699",
+          },
+        },
+        {
+          skillName: "BootStrap",
+          fontAwesomeClassname: "logos-bootstrap",
+          style: {
+            color: "#CC6699",
+          },
+        },
+        {
+          skillName: "JavaScript",
+          fontAwesomeClassname: "simple-icons:javascript",
+          style: {
+            backgroundColor: "#000000",
+            color: "#F7DF1E",
+          },
+        },
+        {
+          skillName: "ReactJS",
+          fontAwesomeClassname: "simple-icons:react",
+          style: {
+            color: "#61DAFB",
+          },
+        },
+        {
+          skillName: "NodeJS",
+          fontAwesomeClassname: "simple-icons:node-dot-js",
+          style: {
+            color: "#339933",
+          },
+        },
+        {
+          skillName: "NPM",
+          fontAwesomeClassname: "simple-icons:npm",
+          style: {
+            color: "#CB3837",
+          },
+        },
+        {
+          skillName: "PHP",
+          fontAwesomeClassname: "simple-icons:php",
+          style: {
+            color: "blue",
+          },
+        },
+        {
+          skillName: "Laravel",
+          fontAwesomeClassname: "simple-icons:laravel",
+          style: {
+            color: "red",
+          },
+        },
+        {
+          skillName: "JAVA",
+          fontAwesomeClassname: "simple-icons:java",
+          style: {
+            color: "#CB3837",
+          },
+        },
+        {
+          skillName: "Python",
+          fontAwesomeClassname: "logos:python",
+          style: {
+            color: "transparent",
+          },
+        },
+        {
+          skillName: "Django",
+          fontAwesomeClassname: "simple-icons:django",
+          style: {
+            color: "lightblue",
+          },
+        },
+        {
+          skillName: "Flask",
+          fontAwesomeClassname: "simple-icons:flask",
+          style: {
+            color: "red",
+          },
+        },
+        {
+          skillName: "Wordpress",
+          fontAwesomeClassname: "simple-icons:wordpress",
+          style: {
+            color: "#1f6f93;",
+          },
+        },
+        {
+          skillName: "Mysql",
+          fontAwesomeClassname: "logos:mysql",
+          style: {
+            color: "transparent",
+          },
+        },
+      ],
+    },
+
+    {
+      title: "UI/UX Design",
+      fileName: "DesignImg",
+      skills: [
+        "⚡ Designing highly attractive user interfaces for mobile and web applications",
+        "⚡ Customizing logo designs and building logos from scratch",
+        "⚡ Creating the flow of application functionalities to optimize user experience",
+        "⚡ Applying latest Design principles and experienced in product wireframing",
+      ],
+      softwareSkills: [
+        {
+          skillName: "Adobe XD",
+          fontAwesomeClassname: "simple-icons:adobexd",
+          style: {
+            color: "#FF2BC2",
+          },
+        },
+        {
+          skillName: "Adobe Illustrator",
+          fontAwesomeClassname: "simple-icons:adobeillustrator",
+          style: {
+            color: "#FF7C00",
+          },
+        },
+        {
+          skillName: "Adobe Photoshop",
+          fontAwesomeClassname: "simple-icons:adobephotoshop",
+          style: {
+            color: "#2fa3f7",
+          },
+        },
+        {
+          skillName: "Adobe Premiere",
+          fontAwesomeClassname: "simple-icons:adobepremiere",
+          style: {
+            color: "#FF2BC2",
+          },
+        },
+        {
+          skillName: "Adobe after effects",
+          fontAwesomeClassname: "simple-icons:adobeaftereffects",
+          style: {
+            color: "purple",
+          },
+        },
+        {
+          skillName: "Figma",
+          fontAwesomeClassname: "simple-icons:figma",
+          style: {
+            color: "#F24E1E",
+          },
+        },
+      ],
+    },
+
+    {
+      title: "Bioinformatician",
+      fileName: "DataScienceImg",
+      skills: [
+        "⚡ Scripting",
+        "⚡ Good Laboratory Practice",
+      ],
+      softwareSkills: [
+        {
+          skillName: "Good Laboratory Practice (GLP)",
+          fontAwesomeClassname: "medical-icon:i-laboratory",
+          style: {
+            backgroundColor: "transparent",
+          },
+        },
+        {
+          skillName: "biotechnology",
+          fontAwesomeClassname: "flat-color-icons:biotech",
+          style: {
+            backgroundColor: "transparent",
+          },
+        },
+        {
+          skillName: "Python",
+          fontAwesomeClassname: "logos:python",
+          style: {
+            backgroundColor: "transparent",
+            color: "transparent",
+          },
+        },
+        {
+          skillName: "R",
+          fontAwesomeClassname: "logos:r-lang",
+          style: {
+            backgroundColor: "transparent",
+            color: "#3776AB",
+          },
+        },
+        {
+          skillName: "data analytics",
+          fontAwesomeClassname: "carbon:text-link-analysis",
+          style: {
+            backgroundColor: "transparent",
+          },
+        },
+      ],
+    },
+  ],
+};
+
+// Projects Page
+const projectsHeader = {
+  title: "Projects",
+  description:
+  ""    
+};
+
+//project cards
+// colors:[primary,secondary,success,danger,warning,info,light,dark,]
+const projectcards = {
+  list: [
+    {
+      title: "BuscoPhylo",
+      img_path: "project-06.png",
+      description:
+        "Easy Busco-based phylogenomic analysis tool",
+      tags: [
+        {
+          lang: "PHP",
+          color: "#004782",
+        },
+        {
+          lang: "Python",
+          color: "#820032",
+        },
+        {
+          lang: "Bash scripts",
+          color: "#00a100",
+        },
+        {
+          lang: "HTML5/CSS3",
+          color: "#ff4b4b",
+        },
+      ],
+      link: "https://buscophylo.inra.org.ma",
+      code: "https://github.com/alaesahbou/BuscoPhylo",
+      linkcolor: "white",
+    },
+
+    {
+      title: "cleardrobe",
+      img_path: "project-07.png",
+      description:
+        "E-commerce website",
+      tags: [
+        {
+          lang: "Wordpress",
+          color: "red",
+        },
+        {
+          lang: "php",
+          color: "#004782",
+        },
+      ],
+      link: "https://cleardrobe.co.uk",
+      code: "#",
+      linkcolor: "white",
+    },
+  ],
+};
+
+const resumeleft = {
+  left: {
+    title: ["Experience"],
+    data: [
+      {
+        title: "Developer and Biologist",
+        subtitle: "Regional Center of Agricultural Research of Rabat, National Institute of Agricultural Research (INRA)",
+        date: "February 2022 – Present",
+        content: [
+          "Development of an SSR database",
+          "Creation of an application to visualize genomic files (FASTA, GFF3, GenBank)",
+          "BuscoPhylo : Easy Busco-based phylogenomic analysis tool (https://buscophylo.inra.org.ma/)",
+          ],
+      },
+      {
+        title: "Developer",
+        subtitle: "Borderline-solutions",
+        date: "March 2021 - October 2021",
+        content: [
+          "Full Stack Developer",
+        ],
+      },
+      {
+        title: "Freelancer",
+        subtitle: "Upwork",
+        date: "August 2020 – Present",
+        content: [
+          "Full Stack Developer",
+        ],
+      },
+    ],
+  },
+};
+
+const resumeright = {
+  right: [
+    {
+      title: ["Education"],
+      data: [
+        {
+          title: "Engineering degree | Computer engineer",
+          subtitle: "High-Tech School",
+          date: "2022 - 2025",
+          content: [],
+        },
+        {
+          title: "LST | Biological analyzes and quality control",
+          subtitle: "Hassan II University Casablanca Faculty of Technical Sciences Mohammedia",
+          date: "2021 - 2022",
+          content: [],
+        },
+        {
+          title: "Specialized technician, IT development",
+          subtitle: "Pro systems salé",
+          date: "2020 - 2022",
+          content: [],
+        },
+        {
+          title: "DEUST | Biology, chemistry and geology",
+          subtitle: "Hassan II University Casablanca Faculty of Technical Sciences Mohammedia",
+          date: "2021 - 2022",
+          content: [],
+        },
+        {
+          title: "Baccalaureate | SVT",
+          subtitle: "Safwa high school",
+          date: "2018 - 2019",
+          content: [],
+        },
+      ],
+    },
+    {
+      title: ["Freelance Work"],
+      data: [
+        {
+          title: "Remove malware from Websites",
+          subtitle: "grasshopperevents.com",
+          content: [
+            'Client Review : "Alae is the perfect developer - quick, knowledgeable and trustworthy. He immediately knew what was needed and did it perfectly. I wish he could work for me full-time!"',
+          ],
+        },
+        {
+          title: "Wordpress Website",
+          subtitle: "cleardrobe.co.uk",
+          content: [
+            'Client Review : "Alae has done a great job for us, he literally saved and restored all our websites, in fact he went the extra mile and even rebuilt the entire website that got corrupted due to the virus, without asking for any extra money. He is an amazing person and a pure talent, we will definitely work with him again and you will not regret working with him he is that amazing. Recommended"',
+          ],
+        },
+      ],
+    },
+  ],
+};
+
+//certificate cards
+const certifications = {
+  certifications: [
+    {
+      title: "Technical Support Fundamentals Google",
+      subtitle: "Google",
+      logo_path: "google.png",
+      certificate_link:
+        "https://www.coursera.org/account/accomplishments/verify/3HD7WN9YWH83",
+      alt_name: "Google",
+      color_code: "#000000",
+    },
+    {
+      title: "Digital Marketing Certificate Google",
+      subtitle: "Google",
+      logo_path: "google.png",
+      certificate_link:
+        "https://learndigital.withgoogle.com/digitalskills/validate-certificate-code",
+      alt_name: "Google",
+      color_code: "#000000",
+    },
+    {
+      title: "IT Security: Defense against the digital dark arts Google",
+      subtitle: "Google",
+      logo_path: "google.png",
+      certificate_link: "https://www.coursera.org/account/accomplishments/verify/M8XYKFPGN8YU",
+      alt_name: "Google",
+      color_code: "#000000",
+    },
+    {
+      title: "IBM",
+      subtitle: "IBM",
+      logo_path: "ibm.png",
+      certificate_link: "https://www.coursera.org/account/accomplishments/verify/74Y9UQP2BSP2",
+      alt_name: "IBM",
+      color_code: "#000000",
+    },
+    {
+      title: "JavaScript : Défis code LinkedIn",
+      subtitle: "LinkedIn",
+      logo_path: "linkedin.png",
+      certificate_link:
+        "https://www.linkedin.com/learning/certificates/701c14ae37eaed5df2cee3b65853c1b802aa59a8dc69225a1ef04e8596031ea2?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_certifications_details%3BV9K7Ao9PQziqPAsxBXnTug%3D%3D",
+      alt_name: "LinkedIn",
+      color_code: "#000000",
+    },
+    {
+      title: "HTML",
+      subtitle: "Sololearn",
+      logo_path: "sololearn.png",
+      certificate_link:
+        "https://www.sololearn.com/Certificate/CT-0N9VU8VG/pdf",
+      alt_name: "Sololearn",
+      color_code: "#000000",
+    },
+    {
+      title: "Python Program From UST",
+      subtitle: "Coursera",
+      logo_path: "coursera-01.png",
+      certificate_link:
+        "https://www.coursera.org/account/accomplishments/verify/AFDV5UUWWG4J",
+      alt_name: "Coursera",
+      color_code: "#000000",
+    },
+    {
+      title: "ATTESTATION DE SUIVI AVEC SUCCÈS",
+      subtitle: "FunMOOC",
+      logo_path: "funmooc.png",
+      certificate_link:
+        "https://drive.google.com/file/d/1fzMl5cnfy-BKyzhZQLj-OFQEigvb6YEL/view",
+      alt_name: "FunMOOC",
+      color_code: "#000000",
+    },
+  ],
+};
+
+// Contact Page
+const contactPageData = {
+  contactSection: {
+    title: "Contact Me",
+    profile_image_path: "contact.jpg",
+    description:
+      "I am available on almost every social media. You can message me, I will reply within 24 hours.",
+  },
+  addressSection: {
+    title: "Address",
+    subtitle: "Bettana Salé, morocco",
+  },
+  phoneSection: {
+    title: "Phone Number",
+    subtitle: "+212 653422557",
+  },
+};
+
+//contact icons
+const socialMediaLinks = [
+  {
+    name: "Github",
+    link: "https://github.com/alaesahbou",
+    iconifyClassname: "simple-icons:hackerrank",
+    fontAwesomeIcon: "fa-github", // Reference https://fontawesome.com/icons/github?style=brands
+    backgroundColor: "white", // Reference https://simpleicons.org/?q=github
+  },
+  {
+    name: "LinkedIn",
+    link: "https://www.linkedin.com/in/alae-eddine-sahbou/",
+    fontAwesomeIcon: "fa-linkedin-in", // Reference https://fontawesome.com/icons/linkedin-in?style=brands
+    backgroundColor: "#0077B5", // Reference https://simpleicons.org/?q=linkedin
+  },
+  {
+    name: "Gmail",
+    link: "mailto:alae.shb147@gmail.com",
+    fontAwesomeIcon: "fa-google", // Reference https://fontawesome.com/icons/google?style=brands
+    backgroundColor: "#D14836", // Reference https://simpleicons.org/?q=gmail
+  },
+  {
+    name: "Instagram",
+    link: "https://www.instagram.com/alaesahbou/",
+    fontAwesomeIcon: "fa-instagram", // Reference https://fontawesome.com/icons/instagram?style=brands
+    backgroundColor: "#E4405F", // Reference https://simpleicons.org/?q=instagram
+  },
+];
+
+export {
+  settings,
+  seo,
+  greeting,
+  socialMediaLinks,
+  skills,
+  competitiveSites,
+  projectsHeader,
+  contactPageData,
+  projectcards,
+  certifications,
+  resumeleft,
+  resumeright,
+};
